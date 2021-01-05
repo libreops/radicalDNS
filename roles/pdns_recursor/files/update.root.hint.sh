9f1dd966f3274a33a530e21c791458694c11f26a
Nikos Roussos <nikos@roussos.cc>
2021-01-05 14:01:00 +0200

    Adjust shell scripts for shellcheck

--- roles/pdns_recursor/files/update.root.hint.sh
+++ roles/pdns_recursor/files/update.root.hint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Get OpenNic Tier1 NS
-dig . NS @ns0.opennic.glue | egrep -v '^;|^$' | sort -u -V | column -t > /tmp/root.hint
+dig . NS @ns0.opennic.glue | grep -Ev '^;|^$' | sort -u -V | column -t > /tmp/root.hint
 
 # Get diff
 diff -q <(sort -V /etc/powerdns/root.hints | column -t) /tmp/root.hint
 DIFF_STATUS=$?
 
 # Get Lines
-LINES=`grep -c ^ /tmp/root.hint`
+LINES=$(grep -c ^ /tmp/root.hint)
 
 # Check & restart if needed, at least 3 NS must exist
-if [ "${LINES}" -gt "8" -a "${DIFF_STATUS}" != "0" ]; then
+if [ "${LINES}" -gt "8" ] && [ "${DIFF_STATUS}" != "0" ]; then
     mv -f /tmp/root.hint /etc/powerdns/root.hints && systemctl restart pdns-recursor
 fi
